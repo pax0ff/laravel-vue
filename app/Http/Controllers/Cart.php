a0dd477aa2f26fb3ed0a994386801ad399a318bf
Stefan Smarandache <stefan.smarandache96@gmail.com>
2022-08-04 16:01:16 +0300

    Add db tables:cart,orders,current user cart,forgotten orders

--- app/Http/Controllers/Cart.php
+++ app/Http/Controllers/Cart.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart as CartModel;
 
 class Cart extends Controller
 {
-
+    public $cartModel = null;
+        public function __construct() {
+            $cartModel = new \App\Models\Cart();
+            return $cartModel;
+        }
 }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,7 +2,28 @@
 
 namespace App\Models;
 
-class Cart
-{
+use Illuminate\Database\Eloquent\Model;
 
+class Cart extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getCartProduct() {
+
+    }
+
+    public function getCurrentUserSession() {
+
+    }
+
+    public function addProductToCart() {
+
+    }
+
+    public function totalCartValue() {
+
+    }
 }
